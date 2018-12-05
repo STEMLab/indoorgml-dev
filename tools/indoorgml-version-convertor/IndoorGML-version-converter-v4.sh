acf7f252302ce1dfdd9201d54b4158058b5539ac
suheeeee <lalune1120@hotmail.com>
2018-12-05 12:07:22 +0900

    update v4

--- tools/indoorgml-version-convertor/IndoorGML-version-converter-v4.sh
+++ tools/indoorgml-version-convertor/IndoorGML-version-converter-v4.sh
@@ -23,11 +23,15 @@ sed 's/<geometry2D>/<cellSpaceBoundaryGeometry>\n\t\t\t\t\t<geometry2D>/' tmp1.g
 rm -f tmp1.gml
 sed 's/<\/geometry3D>/<\/geometry3D>\n\t\t\t\t\t<\/cellSpaceBoundaryGeometry>/' tmp2.gml > tmp1.gml
 rm -f tmp2.gml
-sed 's/<\/geometry2D>/<\/geometry2D>\n\t\t\t\t\t<\/cellSpaceBoundaryGeometry>/' tmp1.gml > converted.gml
-
+sed 's/<\/geometry3D>/<\/geometry3D>\n\t\t\t\t\t<\/cellSpaceBoundaryGeometry>/' tmp1.gml > tmp2.gml
+rm -f tmp1.gml
+sed 's/<\/geometry2D>/<\/geometry2D>\n\t\t\t\t\t<\/cellSpaceBoundaryGeometry>/' tmp2.gml > tmp1.gml
+rm -f tmp2.gml
+sed 's/xsi:schemaLocation="[^"]*/xmlns:navi="http:\/\/www.opengis.net\/indoorgml\/1.0\/navigation" xsi:schemaLocation="http:\/\/www.opengis.net\/indoorgml\/1.0\/core http:\/\/schemas.opengis.net\/indoorgml\/1.0\/indoorgmlcore.xsd http:\/\/www.opengis.net\/indoorgml\/1.0\/navigation http:\/\/schemas.opengis.net\/indoorgml\/1.0\/indoorgmlnavi.xsd/' tmp1.gml > converted.gml
 rm -f tmp1.gml
 
 endT=$(date +%s)
 echo "IndoorGML version convert complete."
 echo "It takes $(($endT-$startT)) seconds :)"
 
+sleep 5
